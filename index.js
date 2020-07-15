1d0a10d90ccf5ba436b5e51f0eec2d37381b267a
Scott Hooper <speedglide@comcast.net>
2020-07-15 16:08:03 -0400

    changed to 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World Master Edit 1!");
+    response.end("Hello World Master Edit 2!");
 });
 
 const port = process.env.PORT || 1337;
