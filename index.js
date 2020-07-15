6ff76671528d50d6abfcd6fb0161460c8734dd9f
Scott Hooper <speedglide@comcast.net>
2020-07-15 16:24:03 -0400

    fixes AB#35

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World Master Edit 2!");
+    response.end("Hello World Master Edit 3!");
 });
 
 const port = process.env.PORT || 1337;
