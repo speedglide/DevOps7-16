49a46e1be1fbf9a739a0fbbb218f51df96bdeb91
Scott Hooper <speedglide@comcast.net>
2020-07-16 08:36:15 -0400

    Reset Demo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World Master Edit 4!");
+    response.end("Hi");
 });
 
 const port = process.env.PORT || 1337;
